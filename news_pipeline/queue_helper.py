0bb629617ce39c791e6ad98d64eb97837d0736cd
misspink1011 <xuanwang1118@gmail.com>
2018-03-22 22:56:44 -0400

    complete news-monitor

---
+++ news_pipeline/queue_helper.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+# import common package in parent directory
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+
+from cloudAMQP_client import CloudAMQPClient
+
+SCRAPE_NEWS_TASK_QUEUE_URL = #TODO: use your own config.
+SCRAPE_NEWS_TASK_QUEUE_NAME = #TODO: use your own config.
+
+DEDUPE_NEWS_TASK_QUEUE_URL = #TODO: use your own config.
+DEDUPE_NEWS_TASK_QUEUE_NAME = #TODO: use your own config.
+
+def clearQueue(queue_url, queue_name):
+    scrape_news_queue_client = CloudAMQPClient(queue_url, queue_name)
+
+    num_of_messages = 0
+
+    while True:
+        if scrape_news_queue_client is not None:
+            msg = scrape_news_queue_client.getMessage()
+            if msg is None:
+                print("Cleared %d messages." % num_of_messages)
+                return
+            num_of_messages += 1
+
+
+if __name__ == "__main__":
+    clearQueue(SCRAPE_NEWS_TASK_QUEUE_URL, SCRAPE_NEWS_TASK_QUEUE_NAME)
+    clearQueue(DEDUPE_NEWS_TASK_QUEUE_URL, DEDUPE_NEWS_TASK_QUEUE_NAME)
